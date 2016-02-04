0904988dc7f9b112118eedd6fdbb6eeb5659eaea
cecois <cecmcgee@gmail.com>
2016-02-04 06:52:30 -0500

    done actually

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	
+
+$(".opacity_slider_control").attr('title', 'adjust the China map opacity');
+
+  $(function() {
+    $( document ).tooltip();
+  });
+
+}); //ready
